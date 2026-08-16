68804f247d1f32f90f89e4cc5a2951ba064504d8
Borey-123 <155078299+Borey-123@users.noreply.github.com>
2026-08-16 21:45:16 +0700

    Remove faker dependency from DatabaseSeeder for production compatibility

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,5 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
         ]);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
